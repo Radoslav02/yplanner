6eb7929ffd3e744d3b09183ae3605266029b1bb9
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2024-07-20 12:09:42 +0200

    build fail fixed

--- src/components/AppointmentCard/AppointmentCard.tsx
+++ src/components/AppointmentCard/AppointmentCard.tsx
@@ -2,7 +2,6 @@ import { Appointment } from '../../models/appointment';
 import './AppointmentCard.scss'
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
 
 interface AppointmentCardProps {
     day: string
