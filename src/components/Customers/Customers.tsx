7ccecae0d73f9935fd25543455edffb57e9af117
Radoslav02 <radoslavpavkovg@gmail.com>
2024-07-20 14:10:52 +0200

    Added customer card design

--- src/components/Customers/Customers.tsx
+++ src/components/Customers/Customers.tsx
@@ -1,12 +1,57 @@
-import "./Customers.scss"
+import { useEffect, useState } from "react";
+import NavBar from "../NavBar/NavBar";
+import "./Customers.scss";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { useAuth } from "../context/AuthContext";
+import { toast } from "react-toastify";
 
-import NavBar from "../NavBar/NavBar"
+import CustomerCard from "../CustomerCard/CustomerCard";
 
-export default function Materials() {
+export default function Customers() {
+  const [clientsData, setClientsData] = useState<any>(null);
+
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user && user.uid) {
+      fetchClients();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    console.log(clientsData);
+  }, [clientsData]);
+
+  async function fetchClients() {
+    try {
+      const clientsCollectionRef = collection(db, `users/${user!.uid}/clients`);
+      const clientDocs = await getDocs(clientsCollectionRef);
+      const clientsData = clientDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setClientsData(clientsData);
+    } catch (error) {
+      console.error("Error fetching clients");
+      toast.error("Error fetching clients");
+    }
+  }
+
   return (
-    <div>
-      <h1>Customers</h1>
+    <div className="customers-container">
+
+      
+
+
+      {clientsData?.length &&
+        clientsData.map((client: any) => (
+          <div key={client.id}>
+            <CustomerCard name={client.name} phone={client.phone} />
+          </div>
+        ))}
+
       <NavBar />
     </div>
-  )
+  );
 }
